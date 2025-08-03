83ed8457d0325bfd84e4f954333dd515cd211c03
Anirudh Sevugan <sonicforces207@gmail.com>
2025-08-03 13:18:51 -0500

    Update index.js

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,16 +1,6 @@
-// api/github/webhooks.js
-const { createProbot } = require("probot");
-const appFn = require("../../../app");
+const { createNodeMiddleware, createProbot } = require("probot");
 
+const app = require("../../../app");
 const probot = createProbot();
 
-module.exports = async (req, res) => {
-  try {
-    const handler = await probot.load(appFn);
-    await handler(req, res);
-  } catch (err) {
-    console.error("Error handling webhook:", err);
-    res.statusCode = 500;
-    res.end("Internal Server Error");
-  }
-};
+module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
